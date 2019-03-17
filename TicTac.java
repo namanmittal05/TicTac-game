b12e0059123a5d1a29c939db403cf4977bbd6bb2
dev-elixir <abhisri1997@gmail.com>
2019-03-17 18:41:14 +0530

    first commit

---
+++ TicTac.java
@@ -0,0 +1,14 @@
+/*
+TicTac Game
+__X_|__O_|__X_
+__O_|__X_|__O_
+  X |  O |  X
+*/
+public class TicTac{
+  public static void main(String[] args) {
+    System.out.println("\tTicTac Game By Abhi:");
+    System.out.println("\t  __X_|__O_|__X_");
+    System.out.println("\t  __O_|__X_|__O_");
+    System.out.println("\t    X |  O |  X");
+  }
+}
