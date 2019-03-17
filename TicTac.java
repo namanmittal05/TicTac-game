378e171169b83b6daf5bfb47ba402514cce02f5e
dev-elixir <abhisri1997@gmail.com>
2019-03-17 19:50:08 +0530

    Updates

--- TicTac.java
+++ TicTac.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /*
 TicTac Game
 __X_|__O_|__X_
@@ -6,9 +7,37 @@ __O_|__X_|__O_
 */
 public class TicTac{
   public static void main(String[] args) {
+    Welcome.greet();
+    Game start = new Game();
+    start.inputName();
+    Welcome.greetPlayer();
+    start.show();
+  }
+}
+class Welcome{
+  public static void greet(){
     System.out.println("\tTicTac Game By Abhi:");
     System.out.println("\t  __X_|__O_|__X_");
     System.out.println("\t  __O_|__X_|__O_");
     System.out.println("\t    X |  O |  X");
+  }
+  public static void greetPlayer(){
+    Game call = new Game();
+    System.out.println("Welcome " + " " + call.x + " and " + call.y + "\n" + "Have Fun!");
+  }
+}
+class Game{
+  public String x,y;
+  public void inputName(){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your name Player 1:");
+    String Player1 = input.nextLine();
+    System.out.println("Enter your name Player 2:");
+    String Player2 = input.nextLine();
+    x = Player1;
+    y = Player2;
+  }
+  public void show(){
+    System.out.println("Hi " + " " + x + " and " + y);
   }
 }
